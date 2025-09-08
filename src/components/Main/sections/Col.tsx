a0a559c1ed4d6805244a2a76e47960b0d37f7f52
nozredge <nozredge@proton.me>
2025-09-08 16:47:34 -0300

    migrate deprecated "size" Col component prop

--- src/components/Main/sections/Col.tsx
+++ src/components/Main/sections/Col.tsx
@@ -1,12 +1,22 @@
 import { JSX, ReactNode } from "react";
 
 function Col(
-  { children, size = "is-half" }: { children: ReactNode; size?: string },
+  { children, size = "is-half", modifiers }: {
+    children: ReactNode;
+    size?: string;
+    modifiers?: string;
+  },
 ): JSX.Element {
+  const classnames = modifiers || size || "";
+
+  size && !modifiers
+    ? console.warn(
+      'Prop "size" of Col component is deprecated. Use "modifiers" instead',
+    )
+    : null;
+
   return (
-    <div
-      className={`column ${size} is-flex`}
-    >
+    <div className={`column ${classnames}`}>
       {children}
     </div>
   );
